ea8ffa8a244c8a1b9de8e558575ec0cb8cd1fcea
GildedSeraphim <snathani@protonmail.com>
2025-08-30 18:41:52 -0500

    stuff

---
+++ src/main.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+
+const HelloTriangeApplication = struct {
+    const Self = @This();
+
+    pub fn init() Self {
+        return Self{};
+    }
+
+    pub fn run(self: *Self) !void {
+
+    }
+
+    pub fn deinit(self: *Self) void {
+
+    }
+};
+
+
